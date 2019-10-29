a0819873eae7d73d73ee6b080d2d54b823e1f140
SnoopyKim <nombu71@gmail.com>
2019-10-29 23:17:33 +0900

    #2.1 Reusable Components with JSX+Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Potato from "./Potato";
+
+function Food({ favorite }) {
+  return <h3>I like { favorite }</h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Potato />
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="samgiopsal" />
+      <Food favorite="chukumi" />
     </div>
   );
 }
